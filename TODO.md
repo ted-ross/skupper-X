3b8be41fff18bc1ca727c9cddfc867c3daff7f15
Ted Ross <tross@apache.org>
2024-01-03 14:26:01 -0500

    Management controller now maintains an AMQP connection into each backbone.

--- TODO.md
+++ TODO.md
@@ -4,7 +4,7 @@
 ## Backbone Feature Tasks
  - In initial setup, specify which ingresses are needed on the site
  - ~~In site YAML, include outgoing peer-links to known access points~~
- - Figure out how to establish connectivity from the management controller to the backbone
+ - ~~Figure out how to establish connectivity from the management controller to the backbone~~
  - Implement the link APIs inband to the management controller
  - Demonstrate automatic backbone site bootstrap
  - Review the inband APIs to allow for full link updates at run time (add/remove links, rotate certificates)
--- components/management-controller/src/inband.js
+++ components/management-controller/src/inband.js
@@ -25,9 +25,172 @@ const db   = require('./db.js');
 
 var controller_name;
 var container;
+var tls_ca;
+var tls_cert;
+var tls_key;
+var bb_connections = {};
+
+const rheaHandlers = function() {
+    container.options.enable_sasl_external = true;
+
+    container.on('connection_open', function(context) {
+        Log('Connection to the management access point is open');
+    });
+
+    container.on('receiver_open', function(context) {
+    });
+
+    container.on('sendable', function(context) {
+    });
+
+    container.on('message', function (context) {
+    });
+}
+
+const createConnection = function(bbid, row) {
+    bb_connections[bbid] = {
+        toDelete: false,
+        host: row.hostname,
+        port: row.port,
+    };
+
+    Log(`Connecting to Access Point: ${row.hostname}:${row.port}`);
+    bb_connections[bbid].conn = container.connect({
+        host: row.hostname,
+        hostname: row.hostname,
+        transport: 'tls',
+        port: row.port,
+        ca:   tls_ca,
+        key:  tls_key,
+        cert: tls_cert,
+    });
+}
+
+const deleteConnection = function(bbid) {
+    let conn = bb_connections[bbid];
+    conn.close();
+    delete bb_connections[bbid];
+}
+
+const reconcileBackboneConnections = async function() {
+    var reschedule_delay = 30000;
+    const client = await db.ClientFromPool();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query("SELECT * FROM BackboneAccessPoints WHERE Lifecycle = 'ready' and Kind = 'manage'");
+        let db_rows = {};
+        for (const row of result.rows) {
+            if (!db_rows[row.backbone]) {
+                db_rows[row.backbone] = row;
+            }
+        }
+
+        for (const bbid of Object.keys(bb_connections)) {
+            bb_connections[bbid].toDelete = true;
+        }
+
+        for (const [bbid, row] of Object.entries(db_rows)) {
+            if (bb_connections[bbid]) {
+                bb_connections[bbid].toDelete = false;
+            } else {
+                createConnection(bbid, row);
+            }
+        }
+
+        for (const bbid of Object.keys(bb_connections)) {
+            if (bb_connections[bbid].toDelete) {
+                deleteConnection(bbid);
+            }
+        }
+
+        await client.query('COMMIT');
+    } catch (err) {
+        Log(`Rolling back reconcile-backbone-connections transaction: ${err.stack}`);
+        await client.query('ROLLBACK');
+        reschedule_delay = 10000;
+    } finally {
+        client.release();
+        setTimeout(reconcileBackboneConnections, reschedule_delay);
+    }
+}
+
+const resolveTLSData = async function() {
+    var reschedule_delay = 1000;
+    const client = await db.ClientFromPool();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query("SELECT * FROM ManagementControllers WHERE Name = $1 and LifeCycle = 'ready'", [controller_name]);
+        if (result.rowCount == 1) {
+            const tls_result = await client.query("SELECT ObjectName FROM TlsCertificates WHERE Id = $1", [result.rows[0].certificate]);
+            if (tls_result.rowCount == 1) {
+                const secret = await kube.LoadSecret(tls_result.rows[0].objectname);
+                let   count  = 0;
+                for (const [key, value] of Object.entries(secret.data)) {
+                    if (key == 'ca.crt') {
+                        tls_ca = Buffer.from(value, 'base64');
+                        count += 1;
+                    } else if (key == 'tls.crt') {
+                        tls_cert = Buffer.from(value, 'base64');
+                        count += 1;
+                    } else if (key == 'tls.key') {
+                        tls_key = Buffer.from(value, 'base64');
+                        count += 1;
+                    }
+                }
+
+                if (count != 3) {
+                    throw(Error(`Unexpected set of values from TLS secret data - expected 3, got ${count}`));
+                }
+
+                reschedule_delay = -1;
+                setTimeout(reconcileBackboneConnections, 0);
+            } else {
+                throw(Error(`Expected to find a TlsCertificate record for ready controller: ${result.rows[0].certificate}`));
+            }
+        }
+        await client.query('COMMIT');
+    } catch (err) {
+        Log(`Rolling back resolve-management-controller transaction: ${err.stack}`);
+        await client.query('ROLLBACK');
+        reschedule_delay = 10000;
+    } finally {
+        client.release();
+        if (reschedule_delay >= 0) {
+            setTimeout(resolveTLSData, reschedule_delay);
+        }
+    }
+}
+
+const resolveControllerRecord = async function() {
+    var reschedule_delay = -1;
+    const client = await db.ClientFromPool();
+    try {
+        await client.query('BEGIN');
+        const result = await client.query("SELECT * FROM ManagementControllers WHERE Name = $1", [controller_name]);
+        if (result.rowCount == 1) {
+            setTimeout(resolveTLSData, 0);
+        } else {
+            client.query("INSERT INTO ManagementControllers (Name) VALUES ($1)", [controller_name]);
+            setTimeout(resolveTLSData, 1000);
+            Log(`No management controller found for '${controller_name}', created new record`);
+        }
+        await client.query('COMMIT');
+    } catch (err) {
+        Log(`Rolling back resolve-management-controller transaction: ${err.stack}`);
+        await client.query('ROLLBACK');
+        reschedule_delay = 10000;
+    } finally {
+        client.release();
+        if (reschedule_delay >= 0) {
+            setTimeout(resolveControllerRecord, reschedule_delay);
+        }
+    }
+}
 
 exports.Start = async function(name, rhea) {
     Log(`[In-band communication starting for controller: ${name}]`);
     controller_name = name;
     container       = rhea;
+    rheaHandlers();
+    await resolveControllerRecord();
 }
