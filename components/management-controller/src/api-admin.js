06f39d8081e76b7282a59d06b519d2db82652fde
Ted Ross <tross@apache.org>
2024-01-31 11:44:03 -0500

    Added first POST handler for the admin API.

--- components/management-controller/src/api-admin.js
+++ components/management-controller/src/api-admin.js
@@ -19,15 +19,57 @@
 
 "use strict";
 
-const db  = require('./db.js');
-const Log = require('./common/log.js').Log;
+const formidable = require('formidable');
+const db         = require('./db.js');
+const Log        = require('./common/log.js').Log;
 
 const API_PREFIX = '/api/v1alpha1/';
+
+const validateFields = function(fields, table) {
+    var optional = {};
+    for (const [key, value] of Object.entries(table)) {
+        optional[key] = value.optional;
+    }
+
+    for (const [key, value] of Object.entries(fields)) {
+        if (Object.keys(table).indexOf(key) < 0) {
+            return `Unknown field key ${key}`;
+        }
+        delete optional[key];
+        switch (table[key].type) {
+        case 'string' :
+            if (typeof value != 'string') {
+                return `Expected string value for key ${key}`;
+            }
+            break;
+
+        case 'bool' :
+            if (typeof value != 'string' || (value != 'true' && value != 'false')) {
+                return `Expected boolean string for key ${key}`;
+            }
+            break;
+
+        case 'number' :
+            if (typeof value != 'string' || isNaN(value)) {
+                return `Expected numeric string for key ${key}`;
+            }
+            break;
+        }
+    }
+
+    for (const [key, value] of Object.entries(optional)) {
+        if (!value) {
+            return `Mandatory key ${key} not found`;
+        }
+    }
+
+    return null;
+}
 
 const listBackbones = async function(res) {
     const client = await db.ClientFromPool();
     try {
-        const result = await client.query("SELECT Id, Name, Lifecycle, Failure FROM Backbones");
+        const result = await client.query("SELECT Id, Name, Lifecycle, Failure, MultiTenant FROM Backbones");
         var list = [];
         result.rows.forEach(row => {
             list.push(row);
@@ -39,6 +81,41 @@ const listBackbones = async function(res) {
     } finally {
         client.release();
     }
+}
+
+const postBackbone = async function(req, res) {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function(err, fields, files) {
+        if (err != null) {
+            Log(err)
+            res.status(400).json({ message: err.message });
+            return;
+        }
+
+        const problem = validateFields(fields, {
+            'name'        : {type: 'string', optional: false},
+            'multitenant' : {type: 'bool',   optional: true},
+        });
+
+        if (problem) {
+            res.status(400).json({ message: problem });
+        } else {
+            const client = await db.ClientFromPool();
+            try {
+                await client.query("BEGIN");
+                const result = await client.query("INSERT INTO Backbones(Name, MultiTenant) VALUES ($1, $2) RETURNING Id", [fields.name, !fields.multitenant || fields.multitenant == 'true']);
+                await client.query("COMMIT");
+
+                res.status(201).json({id: result.rows[0].id});
+                //res.status(201).end();
+            } catch (error) {
+                await client.query("ROLLBACK");
+                res.status(500).send(error.message);
+            } finally {
+                client.release();
+            }
+        }
+    });
 }
 
 const listBackboneSites = async function(bid, res) {
@@ -110,6 +187,10 @@ const listInvitations = async function(res) {
     client.release();
 }
 
+const apiLog = function(req) {
+    Log(`AdminAPI: ${req.ip} - ${req.method} ${req.originalUrl}`);
+}
+
 exports.Initialize = async function(api) {
     Log('[API Admin interface starting]');
 
@@ -117,13 +198,20 @@ exports.Initialize = async function(api) {
     // Backbones
     //
     api.get(API_PREFIX + 'backbones', (req, res) => {
+        apiLog(req);
         listBackbones(res);
+    });
+
+    api.post(API_PREFIX + 'backbones', (req, res) => {
+        apiLog(req);
+        postBackbone(req, res);
     });
 
     //
     // Backbone/Interior Sites
     //
     api.get(API_PREFIX + 'backbone/:bid/sites', (req, res) => {
+        apiLog(req);
         listBackboneSites(req.params.bid, res);
     });
 
@@ -131,6 +219,7 @@ exports.Initialize = async function(api) {
     // Interior Site Links
     //
     api.get(API_PREFIX + 'backbone/:bid/links', (req, res) => {
+        apiLog(req);
         listBackboneLinks(req.params.bid, res);
     });
 
@@ -138,10 +227,12 @@ exports.Initialize = async function(api) {
     // Backbone Access Points
     //
     api.get(API_PREFIX + 'backbonesite/:sid/ingresses', (req, res) => {
+        apiLog(req);
         listSiteIngresses(req.params.sid, res);
     });
 
     api.get(API_PREFIX + 'invitations', (req, res) => {
+        apiLog(req);
         listInvitations(res);
     });
 }
