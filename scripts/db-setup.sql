fda02ef2fb1d5ffde8a718dbd46122254af7aec9
Ted Ross <tross@apache.org>
2025-03-10 16:36:07 -0400

    Change the interface polarity for ingresses to allow them to be allocated to sites.

--- scripts/db-setup.sql
+++ scripts/db-setup.sql
@@ -382,7 +382,7 @@ INSERT INTO BlockTypes (Name, AllowNorth, AllowSouth, AllocateToSite) VALUES
     ('skupperx.io/component', true,  false, true),
     ('skupperx.io/connector', false, true,  false),
     ('skupperx.io/mixed',     true,  true,  false),
-    ('skupperx.io/ingress',   false, true,  false),
+    ('skupperx.io/ingress',   true,  false, true),
     ('skupperx.io/egress',    false, true,  false);
 
 INSERT INTO InterfaceRoles (Name) VALUES
